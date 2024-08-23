a95d9182941d7a826c2c7962723111c6af0b2c1e
k4in <k4in>
2024-08-23 22:27:06 +0200

    added Bears component, added some debugging clgs

---
+++ src/components/Bears.tsx
@@ -0,0 +1,17 @@
+import Bear from '../components/Bear';
+
+import { useBearStore } from '../store/useBearStore';
+
+export default function Bears() {
+  const bears = useBearStore((state) => state.bears);
+
+  return (
+    <main className="mt-10">
+      {bears.length > 0 ? (
+        bears.map((bear) => <Bear bear={bear} key={bear.name} />)
+      ) : (
+        <div>Click "New Bear" to add your first bear!</div>
+      )}
+    </main>
+  );
+}
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,23 +1,16 @@
 import Buttons from '../components/Buttons';
-import Bear from '../components/Bear';
 
-import { useBearStore } from '../store/useBearStore';
+import Bears from '../components/Bears';
 
 export default function HomePage() {
-  const bears = useBearStore((state) => state.bears);
+  console.log('[re-rendering HomePage]');
   // const increase = useBearStore((state) => state.increase);
 
   return (
     <div className="flex gap-3 flex-col items-center mt-3 px-20">
       <Buttons />
       <div className="h-[1px] bg-blue-500 w-3/4"></div>
-      <main className="mt-10">
-        {bears.length > 0 ? (
-          bears.map((bear) => <Bear bear={bear} key={bear.name} />)
-        ) : (
-          <div>Click "New Bear" to add your first bear!</div>
-        )}
-      </main>
+      <Bears />
     </div>
   );
 }
